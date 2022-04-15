789d0ed7e2f743fa6e8aacd08e77023d8024c3b6
cyanrad <cyanrad@outlook.com>
2022-04-15 21:14:50 +0400

    idk

--- NeuralNetwork/applyMode.py
+++ NeuralNetwork/applyMode.py
@@ -1,17 +1,23 @@
-import numpy as np          # for the matrix, and arrays
-import cv2 as cv            # img reading and resizing
-import tensorflow as tf
-from tensorflow import keras
+import numpy as np                  # for the matrix, and arrays
+import cv2 as cv                    # img reading and resizing
+from tensorflow import keras        # Interface of the tensorflow NN libraries
+import tensorflow as tf             # ML and AI library
 
+# >> disabling the graphics card
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+# checking which device is used to compute the neural net
 if tf.test.gpu_device_name():
     print('GPU found')
 else:
     print("No GPU found")
 
-THREASHOLD = 1
+# >> the threshold value for the neural network
+# The neural network can output values ranging from -3 to 3
+# setting the threshold will tell the edge detector which
+# value is the minimum acceptable one
+THRESHOLD = 1
 
 
 def scale_down(img, max_height=200):
@@ -44,6 +50,7 @@ def scale_up(img, original_shape):
     return img
 
 
+# >> the function that does the edge detection on the whole image
 def detect_edges(img, model):
     # creating an empty image
     returnImg = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
@@ -60,39 +67,44 @@ def detect_edges(img, model):
                 img[i-1, j+1], img[i, j-1], img[i, j+1],
                 img[i+1, j-1], img[i+1, j], img[i+1, j+1]])
 
-            prediction = model(temp)
-            if(prediction[0][0] > THREASHOLD):                      # detecting if edge
-                returnImg[i, j] = (255, 255, 255)           # draw a white dot
+            temp = temp-img[i, j]     # getting the contrast
+            prediction = model(temp)  # passing the data to the model
+            if(prediction[0][0] > THRESHOLD):      # detecting if edge (deps on threshold)
+                returnImg[i, j] = (255, 255, 255)   # draw a white dot
     print("done")
     return returnImg
 
 
-reloaded = tf.keras.models.load_model('dnn_model_32_con')
-
-# for simplicity
-
-
+# >> Displays an image and waits for user input
 def show_and_wait(img, window="Testing"):
     cv.imshow(window, img)
     cv.waitKey(0)
 
 
-# reading the image and displaying it
+# >> start of the porgram
+# >> loading the neural network model
+reloaded = tf.keras.models.load_model('dnn_model_32_con')
+
+
+# >> reading the image and displaying it
 img = cv.imread("images/pepper.bmp")
 original_shape = img.shape[0:2]  # holding the orignal shape for later
 show_and_wait(img)
 
-# converting the image to grayscale, displaying, and compressing it.
+# >> converting the image to grayscale, displaying, and compressing it.
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 show_and_wait(img_gray)
 #img_gray = scale_down(img_gray, 200)
 
+# >> doing the edge detection
 edge = detect_edges(img_gray, reloaded)
+# won't effect the image if no downscaling is done
 edge = scale_up(edge, original_shape)
 
+# >> writing the image to a file
 cv.imwrite("face2.jpg", edge)
 
+# >> displaying the image
 show_and_wait(edge)
-
 
 cv.destroyAllWindows()
--- NeuralNetwork/deepConvNeuralNet.py
+++ NeuralNetwork/deepConvNeuralNet.py
@@ -1,28 +1,33 @@
-from tensorflow.keras import layers
-from tensorflow import keras
-import tensorflow as tf
-import matplotlib.pyplot as plt  # math plots
-import numpy as np
-import pandas as pd
-import random
+from tensorflow.keras import layers     # class to create the Dense deep net
+from tensorflow import keras            # Interface of the tensorflow NN libraries
+import tensorflow as tf                 # ML and AI library
+import numpy as np                      # arrays and matrices
+import pandas as pd                     # to read the training data
+import random                           # to acquire data from the data set randomly
 
+# >> disabling the graphics card
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+# checking which device is used to compute the neural net
 if tf.test.gpu_device_name():
     print('GPU found')
 else:
     print("No GPU found")
 
 
+# >> Creating the Neural Net model
 def build_and_compile_model(norm, out):
+    # creating the densly connected deep conv neural network
     model = keras.Sequential([
         norm,
         layers.Dense(64, activation='elu'),
         layers.Dense(64, activation='elu'),
-        layers.Dense(1)
+        layers.Dense(32, activation='elu'),
+        out
     ])
 
+    # compiling the model
     model.compile(loss='mean_absolute_error',
                   optimizer=tf.keras.optimizers.Adam(0.001))
     return model
@@ -33,12 +38,14 @@ np.set_printoptions(precision=3, suppress=True)
 
 n = 44836  # number of records in file
 s = 4000  # desired sample size
-# , skiprows=skip
 column_names = ['P0', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'edge']
+# used for getting random data from the set
+# use this when we don't want the entire data set
 skip = sorted(random.sample(range(n), n-s))
+
+# skiprows=skip to use skip
 raw_dataset = pd.read_csv('testing.csv', names=column_names, skiprows=skip)
 dataset = raw_dataset.copy()
-
 
 train_dataset = dataset.sample(frac=0.8, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
@@ -55,17 +62,16 @@ p_normalizer.adapt(train_features)
 o_normalizer = layers.Normalization(input_shape=[1, ], axis=None)
 o_normalizer.adapt(train_labels)
 
+# >> creating and Training the model
 p_model = build_and_compile_model(p_normalizer, o_normalizer)
 history = p_model.fit(
-    train_features,
-    train_labels,
-    epochs=10000,
-    # Suppress logging.
-    verbose=2,
+    train_features,  # the inputs to  the model
+    train_labels,   # the labels we want the model to train for
+    epochs=10000,   # epoch count
+    verbose=2,      # Log Progress
     # Calculate validation results on 20% of the training data.
-    validation_split=0.2)
-p_model.save('dnn_model_32_con')
-test_predictions = p_model.predict(
-    np.array([102, 100, 106, 104, 104, 113, 108, 101]))
+    validation_split=0.4)
 
-plt.show()
+
+# >> saving the model
+p_model.save('dnn_model_32_con')
